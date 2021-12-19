efc771142ad36a74c8c0291c518cb02e550b04ef
MinhBuiCong <62849019+MinhBuiCong@users.noreply.github.com>
2021-12-19 13:49:27 +0100

    remove

--- strapi-api/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'b64531da4ca4cc8c271a99cadca28a0d'),
-    },
-  },
-});
--- strapi-api/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'd2ef1600-7746-41f1-bc14-c431c9194e88'
-};
